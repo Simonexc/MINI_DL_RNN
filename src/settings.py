0e607526778358ec7b52d3f7c3d44ba3ce913eb8
SzymonTrochimiak <91737036+SzymonTrochimiak@users.noreply.github.com>
2024-04-28 13:40:25 +0200

    Upload of dataset

--- src/settings.py
+++ src/settings.py
@@ -2,8 +2,15 @@ from enum import Enum
 
 ENTITY = "dl-mini"
 PROJECT = "DL_PROJECT_RNN"
-NAMED_CLASSES = ["yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"]
-ALL_CLASSES = NAMED_CLASSES + ["unknown", "silence"]
+NAMED_CLASSES = []
+UNKNOWN_CLASS = "unknown"
+ALL_CLASSES = ["yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go", "silence", UNKNOWN_CLASS]
+AUDIO_FILE_METADATA = {
+    "sample_rate": 16000,
+    "encoding": "PCM_S",
+    "bits_per_sample": 16,
+    "num_channels": 1,
+}
 
 
 class JobType(Enum):
@@ -12,3 +19,9 @@ class JobType(Enum):
 
 class ArtifactType(Enum):
     DATASET = "dataset"
+
+
+class SplitType(Enum):
+    TRAIN = "train"
+    VALIDATION = "validation"
+    TEST = "test"
