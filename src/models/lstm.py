9c79fc9408949047f5efbfed52e3ee9863145927
Simonexc <szymekdt@gmail.com>
2024-05-04 09:26:59 +0200

    Update lstm

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -7,18 +7,16 @@ from .embeddings import FCEmbedding
 class LSTMModel(nn.Module):
     def __init__(
         self,
-        input_size: int,
-        embedding_hidden: int,
-        hidden_vector_size: int,
+        num_mel_bins: int,
+        hidden_layer_size: int,
         layers_num: int,
         dropout: float = 0.2,
     ):
         super().__init__()
-        self.embedding = FCEmbedding(input_size, hidden_vector_size, embedding_hidden)
         self.lstm = nn.LSTM(
-            hidden_vector_size, hidden_vector_size, layers_num, batch_first=True, dropout=dropout
+            num_mel_bins, hidden_layer_size, layers_num, batch_first=True, dropout=dropout
         )
-        self.linear = nn.Linear(hidden_vector_size, NUM_CLASSES)
+        self.linear = nn.Linear(hidden_layer_size, NUM_CLASSES)
 
         self.init_weights()
 
@@ -28,7 +26,6 @@ class LSTMModel(nn.Module):
         self.linear.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, src: Tensor) -> Tensor:
-        src = self.embedding(src)
         output, (hidden, _) = self.lstm(src)
         output = output.mean(dim=1)
         output = self.linear(output)
